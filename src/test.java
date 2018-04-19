3df20960421ea95aef8b1c16bee8c26436c7b3cd
xujunyan1Git <1245435647@qq.com>
2018-04-19 11:18:37 +0800

    提交test

---
+++ src/test.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+public class test {
+    public static void main(String[] args) {
+        List list = Arrays.asList("a", "b", "b", "c", "a", "b", "c");
+        List listNew = new ArrayList();
+        listNew.addAll(list);
+        for (int i = 0; i < listNew.size(); i++) {
+         if ("b".equals(listNew.get(i))) {
+         listNew.remove(i);
+         }
+        }
+//        Iterator<String> it = listNew.iterator();
+//        while (it.hasNext() ) {
+//String ss=it.next();
+//if(ss.equals("b"))
+//            it.remove();
+//        }
+        System.out.println(listNew);
+
+    }
+}
